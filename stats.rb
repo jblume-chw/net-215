f766b17582f7664266def65006bedce497564943
Jeff <jblume@chw.org>
2011-05-05 14:46:09 -0500

    add count of vowel-started words

--- stats.rb
+++ stats.rb
@@ -3,4 +3,4 @@
 words = File.read('stopwords.txt').split(',')
 puts "Number of words: " + words.size.to_s
 puts "Average word size: I DON'T KNOW RUBY"
-
+puts "Words that start with a vowel: I DON'T KNOW RUBY"
